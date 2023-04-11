2b2fcdbe8b492d066f30adda024649f6353d4d57
Rajesh Pillai <rajesh@algorisys.com>
2023-04-11 11:35:04 +0530

    NuxtLink

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">Nuxt</NuxtLink>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+        </ul>
+      </nav>
+      <a href="/about">About normal link...</a>
+    </header>
     <h2>Home</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos magnam perspiciatis fuga deleniti! Incidunt quod eum voluptates eveniet. Quas, ea?</p>
   </div>
