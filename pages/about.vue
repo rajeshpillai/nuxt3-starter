8c688982360c12b9557d83c8a5ae063c98d56770
Rajesh Pillai <rajesh@algorisys.com>
2023-04-11 11:22:48 +0530

    Add nested routes

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,14 @@
 <template>
-  <h2>About View</h2>
+  <div>
+    <h2>About</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos magnam perspiciatis fuga deleniti! Incidunt quod eum voluptates eveniet. Quas, ea?</p>
+  </div>
 </template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,21 @@
 <template>
-  <h2>Index View</h2>
+  <div>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos magnam perspiciatis fuga deleniti! Incidunt quod eum voluptates eveniet. Quas, ea?</p>
+  </div>
 </template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+
+  p {
+    margin: 20px 0;
+  }
+</style>
