6c619923f5efa12473c4b81af13de37e6d5c065a
Rajesh Pillai <rajesh@algorisys.com>
2023-04-11 12:33:53 +0530

    Reusable components

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -3,7 +3,7 @@
     <h2>Product - ALL</h2>
     <div class="grid grid-cols-4 gap-5">
       <div v-for="p in products" :key="`${p.id}`">
-        <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+        <ProductCard  :product="p"/>
       </div>
 
     </div>
