3569f3e4bcb5ab4c119ffd502e695e20e0dfe2f6
Rajesh Pillai <rajesh@algorisys.com>
2023-04-11 12:47:01 +0530

    Error handling:  fatal:true for handling error in client routing as well

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -10,6 +10,10 @@
 
   const { data: product } = await useFetch(uri, {key: id} );
 
+  if (!product.value) {
+    throw createError({ statusCode: 404, statusMessage: 'Product not found', fatal: true })
+  }
+
 </script>
 
 <style lang="scss" scoped>
